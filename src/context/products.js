7ca4cfc03e9bf70bb1c922e9a6032613345f3e75
unknown <kishore.kukutla@gmail.com>
2021-12-31 19:37:27 +0530

    first commit

--- src/context/products.js
+++ src/context/products.js
@@ -1,6 +1,6 @@
 import React from "react";
-import axios from "axios";
-import url from "../utils/URL";
+//import axios from "axios";
+//import url from "../utils/URL";
 import featuredProducts from "../utils/helpers";
 import data from "../utils/localCart.js";
 export const ProductContext = React.createContext();
